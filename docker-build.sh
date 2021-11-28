7ecb03a3254fec65b862ccdff4acd4e6800210f0
HorusElohim <horuselohim@gmail.com>
2021-11-28 15:59:30 +0100

    Add jetsone specifig dockerfile due to incompatibilities

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
-docker build -t imm -f docker/Dockerfile .
+echo -e "[IMM] Docker build started"
+echo -e "[IMM] Detection architecture..."
+architecture=$(dpkg --print-architecture)
+
+if [[ $architecture == "amd64" ]]; then
+  echo -e "[IMM](amd64)"
+  dockerfile=docker/amd64/Dockerfile
+fi
+
+if [[ $architecture == "arm64" ]]; then
+  echo -e "[IMM](arm64)"
+  dockerfile=docker/arm64/Dockerfile
+fi
+
+echo -e "[IMM] Dockerfile used: $dockerfile"
+docker build -t imm -f $dockerfile .
--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
-docker run -it --gpus all imm
+echo -e "[IMM] Docker build run"
+echo -e "[IMM] Detection architecture..."
+architecture=$(dpkg --print-architecture)
+
+if [[ $architecture == "amd64" ]]; then
+  echo -e "[IMM](amd64)"
+  dockercmd="-it --rm --gpus all imm $@"
+fi
+
+if [[ $architecture == "arm64" ]]; then
+  echo -e "[IMM](arm64)"
+  dockercmd="-it --rm all imm $@"
+fi
+
+echo "[IMM] Running docker with the following cmd: $dockercmd"
+docker run $dockercmd
