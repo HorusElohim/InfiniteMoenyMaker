1b70e9f3bdf3c570b0af8a92f487666e1f42761e
HorusElohim <horuselohim@gmail.com>
2021-11-29 12:22:28 +0100

    Final working docker images for arm/amd 64

--- docker-run.sh
+++ docker-run.sh
@@ -6,12 +6,12 @@ architecture=$(dpkg --print-architecture)
 
 if [[ $architecture == "amd64" ]]; then
   echo -e "[IMM](amd64)"
-  dockercmd="-it --rm --gpus all imm $@"
+  dockercmd="-it --rm --gpus all --network host imm $@"
 fi
 
 if [[ $architecture == "arm64" ]]; then
   echo -e "[IMM](arm64)"
-  dockercmd="-it --rm imm $@"
+  dockercmd="-it --rm --runtime nvidia --network host imm $@"
 fi
 
 echo "[IMM] Running docker with the following cmd: $dockercmd"
